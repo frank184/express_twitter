f4218f0e4a6b5f4d8173c9f6a6038af1fea430d1
frank184 <francoisbelanger1993@hotmail.com>
2016-04-25 17:10:29 -0400

    finished chapter 2, with changes for nodejs v.4 and express v.3

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ var bodyParser = require('body-parser')
 var tweets = []
 
 var app = express();
-app.listen(3000);
+
+app.listen(3000, function() {
+  port = this.address().port;
+  hostname = this.address().address;
+  hostname = hostname === '::' ? 'localhost' : hostname;
+  console.log('[*] Started express_twitter on ' + hostname + ':' + port);
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('Welcome to Node Twitter');
+  var title = 'Chirpie',
+      header = 'Welcome to Chirpie'
+
+  response.render('index', {
+      'title': title,
+      'header': header,
+      'tweets': tweets,
+      stylesheets: ['/public/style.css']
+  });
 });
 
 parser = bodyParser.urlencoded({
@@ -17,10 +34,14 @@ parser = bodyParser.urlencoded({
 app.post('/send', parser, function(request, response) {
   if (request.body && request.body.tweet) {
     tweets.push(request.body.tweet);
-    response.send({
-      status: "ok",
-      message: "Tweet received!"
-    });
+
+    if(acceptsHtml(request.headers['accept']))
+      response.redirect(302, '/');
+    else
+      response.send({
+        status: "ok",
+        message: "Tweet received!"
+      });
   } else {
     response.send({
       status: "nok",
@@ -32,3 +53,11 @@ app.post('/send', parser, function(request, response) {
 app.get("/tweets", function(request, response) {
   response.send(tweets);
 });
+
+function acceptsHtml(header) {
+  var accepts = header.split(',');
+  for (var i = 0; i < accepts.length; i += 1)
+    if (accepts[0] === 'text/html')
+      return true;
+  return false;
+}
